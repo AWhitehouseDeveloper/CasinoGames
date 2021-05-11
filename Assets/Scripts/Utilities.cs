be9396a5e50b6ddc48483dfd0d5815de72f1c493
DakotaBuckner07 <61479565+DakotaBuckner07@users.noreply.github.com>
2021-05-11 17:14:23 -0600

    Betting Beginning

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,10 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class Utilities
 {
     private static Player player;
+
+    [Range(1, 9)]private static int selectedChip = 1;
+    public static Image arrow;
+
+    public static void OnRightArrow()
+    {
+        if(selectedChip != 9) selectedChip++;
+        PositionArrow();
+    }
+
+    public static void OnLeftArrow()
+    {
+        if(selectedChip != 1) selectedChip--;
+        PositionArrow();
+    }
+
+    public static void PositionArrow()
+    {
+        switch(selectedChip)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+        }
+    }
 
     public static void Payout(int bet, int multiplier)
     {
