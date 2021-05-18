20bf4d033520e4f14e5e722ec7ada1eb6c40ead2
RemiixTaZe <Sirdracius926@gmail.com>
2021-05-18 17:14:26 -0600

    More pulled changes

--- Assets/Scripts/Craps/CrapsGame.cs
+++ Assets/Scripts/Craps/CrapsGame.cs
@@ -34,22 +34,27 @@ public class CrapsGame : MonoBehaviour
     private bool EndofRound = false;
     private int point = 0;
     private bool PassWin = false;
+    private int PassBet = 0;
+    private int DontPassBet = 0;
+    private int FieldAmount = 0;
+    private Dictionary<int, int> HardwayBets = new Dictionary<int, int>();
+    private Dictionary<int, int> OneRollBets = new Dictionary<int, int>();
+
     #region MainGameFunction(OnRoll, FlipIndicator, and EndRound)
     public void OnRoll()
     {
         int die1 = Utilities.RollDie();
         int die2 = Utilities.RollDie();
         int roll = die1 + die2;
-        if(FirstRoll)
+        if (FirstRoll)
         {
-
-            if(roll == 2 || roll == 3 || roll == 12)
+            if (roll == 2 || roll == 3 || roll == 12)
             {
                 PassWin = false;
                 EndofRound = true;
                 Debug.Log("Craps");
             }
-            else if(roll == 7 || roll == 11)
+            else if (roll == 7 || roll == 11)
             {
                 PassWin = true;
                 EndofRound = true;
@@ -64,19 +69,22 @@ public class CrapsGame : MonoBehaviour
         }
         else
         {
-            if(roll == point)
+            if (roll == point)
             {
                 PassWin = true;
                 EndofRound = true;
             }
-            else if(roll == 7)
+            else if (roll == 7)
             {
                 PassWin = false;
                 EndofRound = true;
             }
         }
+        if (die1 == die2) Hardways(roll);
+        if(FieldAmount > 0) FieldBets(roll);
+        OneRolls(roll);
         Debug.Log(roll);
-        if(EndofRound)
+        if (EndofRound)
         {
             EndRound();
         }
@@ -84,35 +92,149 @@ public class CrapsGame : MonoBehaviour
 
     public void FlipIndicator(int num)
     {
+        PointIndicator.transform.position = Vector3.zero;
         if (num == -1)
         {
-            PointIndicator.transform.position = new Vector2(230, -55);
+            PointIndicator.transform.position = new Vector2(255, 640);
             PointIndicator.GetComponent<Image>().sprite = IndicatorOff;
         }
         else
         {
             PointIndicator.GetComponent<Image>().sprite = IndicatorOn;
-            if(num == 4)
-                PointIndicator.transform.position = new Vector2(280, -175);
-            else if(num == 5)                    
-                PointIndicator.transform.position = new Vector2(290, -175);
-            else if(num == 6)                    
-                PointIndicator.transform.position = new Vector2(365, -175);
-            else if(num == 8)                    
-                PointIndicator.transform.position = new Vector2(440, -175);
-            else if(num == 9)                    
-                PointIndicator.transform.position = new Vector2(515, -175);
-            else if(num == 10)                   
-                PointIndicator.transform.position = new Vector2(590, -175);
+            if (num == 4)
+                PointIndicator.transform.position = new Vector2(305, 525);
+            else if (num == 5)
+                PointIndicator.transform.position = new Vector2(395, 525);
+            else if (num == 6)
+                PointIndicator.transform.position = new Vector2(495, 525);
+            else if (num == 8)
+                PointIndicator.transform.position = new Vector2(590, 525);
+            else if (num == 9)
+                PointIndicator.transform.position = new Vector2(685, 525);
+            else if (num == 10)
+                PointIndicator.transform.position = new Vector2(780, 525);
         }
     }
 
     public void EndRound()
     {
+        PassBetOut();
         FirstRoll = true;
         EndofRound = false;
         FlipIndicator(-1);
         point = 0;
+    }
+    #endregion
+
+    #region Pass/DontPassBet
+    public void PassBetOut()
+    {
+        if(PassWin)
+        {
+            Utilities.Payout(PassBet, 2);
+        }
+        else
+        {
+            Utilities.Payout(DontPassBet, 2);
+        }
+        PassBet = 0;
+        DontPassBet = 0;
+    }
+    public void PlacePassBet(int amount)
+    {
+        PassBet += amount;
+    }
+    public void PlaceDontPassBet(int amount)
+    {
+        DontPassBet += amount;
+    }
+    #endregion
+
+    #region FieldBets
+    public void FieldBets(int roll)
+    {
+        int multiplier = 1;
+        if (roll == 2) multiplier = 2;
+        else if (roll == 12) multiplier = 3;
+        Utilities.Payout(FieldAmount, multiplier);
+        if (roll == 6 || roll == 7 || roll == 8) FieldAmount = 0;
+    }
+
+    public void PlaceFieldBet(int amount)
+    {
+        FieldAmount += amount;
+    }
+    #endregion
+
+    #region Hardways
+    public void Hardways(int roll)
+    {
+        int multiplier = 1;
+        if(HardwayBets.ContainsKey(-1))
+        {
+            multiplier = 7;
+            int amount = HardwayBets[-1];
+            Utilities.Payout(amount, multiplier);
+        }
+        multiplier = 1;
+        if (HardwayBets.ContainsKey(roll))
+        {
+            if (roll == 4 || roll == 10) multiplier = 7;
+            else if (roll == 6 || roll == 8) multiplier = 9;
+            int amount = HardwayBets[roll];
+            Utilities.Payout(amount, multiplier);
+            HardwayBets.Clear();
+            PlaceHardwayBet(roll, amount);
+        }
+        else HardwayBets.Clear();
+    }
+
+    public void OnClickHardwayBet(int placement)
+    {
+        PlaceHardwayBet(placement, 100);
+        Debug.Log("Hardway Bet Placed on " + placement);
+    }
+
+    public void PlaceHardwayBet(int placement, int amount)
+    {
+        HardwayBets.Add(placement, amount);
+    }
+    #endregion
+
+    #region OneRolls
+    public void OneRolls(int roll)
+    {
+        Dictionary<int, int> temp = new Dictionary<int, int>();
+        int multiplier = 1;
+        if (roll == 7) multiplier = 4;
+        else if (roll == 2 || roll == 12) multiplier = 30;
+        else if (roll == 3 || roll == 11) multiplier = 15;
+
+        for(int i = 0; i < OneRollBets.Count; i++)
+        {
+            if (OneRollBets.ContainsKey(roll))
+            {
+                int amount = OneRollBets[roll];
+                Utilities.Payout(amount, multiplier);
+                temp.Add(roll, amount);
+            }
+        }
+        OneRollBets.Clear();
+        for(int i = 0; i < temp.Count; i++)
+        {
+            OneRollBets = temp;
+        }
+    }
+
+    public void OnClickOneRollBet(int placement)
+    {
+        PlaceOneRollBet(placement, 1000);
+        Debug.Log("OneRoll Bet Placed on " + placement);
+    }
+
+    public void PlaceOneRollBet(int placement, int amount)
+    {
+        OneRollBets.Add(placement, amount);
     }
     #endregion
 }
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,18 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class Utilities
 {
+    public static Sprite FiveThousand;
+    public static Sprite OneThousand;
+    public static Sprite FiveHundred;
+    public static Sprite OneHundred;
+    public static Sprite Fifty;
+    public static Sprite Twenty;
+    public static Sprite Ten;
+    public static Sprite Five;
+    public static Sprite One;
     private static Player player;
 
     public static void Payout(int bet, int multiplier)
     {
         player.Payout((long)bet * multiplier);
+        Debug.Log("Payout of " + (bet * multiplier));
     }
 
     public static int RollDie()
     {
-        return Random.Range(1, 6); 
+        return Random.Range(1, 7); 
     }
 }
